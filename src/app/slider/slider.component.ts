a1b8dff1de294b48376b4e3143f2bccb7ec4283b
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-06-01 13:24:47 +0430

    title slider

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -12,41 +12,41 @@ export class SliderComponent implements OnInit {
       image: "assets/pic/1.jpg",
       thumbImage: "assets/pic/1.jpg",
       alt: 'Image 1',
-      title: 'Image 1'
+      title: '248000'
     }, {
       image: "assets/pic/2.jpg",
       thumbImage: "assets/pic/2.jpg",
-      title: 'Image 2',
+      title: '268000',
       alt: 'Image 2'
     }, {
       image: "assets/pic/3.jpg",
       thumbImage: "assets/pic/3.jpg",
-      title: 'Image 3',
+      title: '268000',
       alt: 'Image 3'
     }, {
       image: "assets/pic/4.jpg",
       thumbImage: "assets/pic/4.jpg",
-      title: 'Image 4',
+      title: '268000',
       alt: 'Image 4'
     }, {
       image: "assets/pic/5.jpg",
       thumbImage: "assets/pic/5.jpg",
-      title: 'Image 5',
+      title: '268000',
       alt: 'Image 5'
     }, {
       image: "assets/pic/6.jpg",
       thumbImage:"assets/pic/6.jpg",
-      title: 'Image 6',
+      title: '268000',
       alt: 'Image 6'
     },{
       image: "assets/pic/7.jpg",
       thumbImage: "assets/pic/7.jpg",
-      title: 'Image 6',
+      title: '268000',
       alt: 'Image 6'
     },{
       image: "assets/pic/8.jpg",
       thumbImage: "assets/pic/8.jpg",
-      title: 'Image 6',
+      title: '268000',
       alt: 'Image 6'
     },{
       image: "assets/pic/9.jpg",
