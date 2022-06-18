d9f4e7903f24913e852e2f55eecc05d851cf2f72
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-06-18 12:43:42 +0430

    i need hint
    I want to show only the existing titles by breaking the two loops

--- src/app/mainlist/mainlist.component.ts
+++ src/app/mainlist/mainlist.component.ts
@@ -12,29 +12,40 @@ import { ModalService } from '../_modal';
 
 
 export class MainlistComponent implements OnInit {
-  clothes = [
-    { title: 1112, images: ["assets/pic/1.jpg", "assets/pic/11.jpg", "assets/pic/1.jpg"], tags: ['بدون چروک', 'دکمه دار', 'جلوبسته'] },
-    { title: 1113, images: ["assets/pic/13.jpg", "assets/pic/10.jpg"], tags: ['زیپی', 'بدون رنگ دهی', ' بدون چروک'] },
-    { title: 1114, images: ["assets/pic/12.jpg", "assets/pic/9.jpg"], tags: ['جلوبسته', 'جیبدار', 'بدون دکمه'] },
-    { title: 1115, images: ["assets/pic/2.jpg", "assets/pic/8.jpg"], tags: ['زیپی', 'ابایی', 'جیبدار'] },
-    { title: 1116, images: ["assets/pic/3.jpg", "assets/pic/7.jpg"], tags: ['زیپی', ' بدون چروک', 'جیبدار'] },
-    { title: 1117, images: ["assets/pic/4.jpg", "assets/pic/6.jpg"], tags: ['زیپی', 'بدون دکمه', 'جلوبسته' , 'جلوبسته'] },
-    { title: 1118, images: ["assets/pic/5.jpg", "assets/pic/5.jpg"], tags: ['زیپی', 'ابایی', 'جیبدار'] },
-    { title: 1119, images: ["assets/pic/6.jpg", "assets/pic/4.jpg"], tags: ['بدون چروک', 'زیپی', 'جلوبسته'] },
-    { title: 1120, images: ["assets/pic/7.jpg", "assets/pic/3.jpg"], tags: ['زیپی', 'جیبدار', 'بدون چروک'] },
-    { title: 1121, images: ["assets/pic/8.jpg", "assets/pic/2.jpg"], tags: ['زیپی', 'بدون چروک', 'جلوبسته'] },
-    { title: 1122, images: ["assets/pic/9.jpg", "assets/pic/1.jpg"], tags: ['بدون چروک', 'جیبدار', 'جلوبسته'] },
-    { title: 1123, images: ["assets/pic/11.jpg", "assets/pic/1.jpg"], tags: ['جلوبسته', 'بدون دکمه', 'زیپی'] }
+  clothes:any = [
+    { title: 1112, images: ["assets/pic/1.jpg", "assets/pic/11.jpg", "assets/pic/1.jpg"], tagId: [2, 4, 5] },
+    { title: 1113, images: ["assets/pic/13.jpg", "assets/pic/10.jpg"], tagId: [1, 4, 7] },
+    { title: 1114, images: ["assets/pic/12.jpg", "assets/pic/9.jpg"], tagId: [3, 5, 1] },
+    { title: 1115, images: ["assets/pic/2.jpg", "assets/pic/8.jpg"], tagId: [1, 4, 6] },
+    { title: 1116, images: ["assets/pic/3.jpg", "assets/pic/7.jpg"], tagId: [2, 3, 6] },
+    { title: 1117, images: ["assets/pic/4.jpg", "assets/pic/6.jpg"], tagId: [1, 7, 4] },
+    { title: 1118, images: ["assets/pic/5.jpg", "assets/pic/5.jpg"], tagId: [3] },
+    { title: 1119, images: ["assets/pic/6.jpg", "assets/pic/4.jpg"], tagId: [2, 1, 3, 5, 6, 7] },
+    { title: 1120, images: ["assets/pic/7.jpg", "assets/pic/3.jpg"], tagId: [4, 3] },
+    { title: 1121, images: ["assets/pic/8.jpg", "assets/pic/2.jpg"], tagId: [1, 2, 3, 4, 5, 6, 7] },
+    { title: 1122, images: ["assets/pic/9.jpg", "assets/pic/1.jpg"], tagId: [2, 5] },
+    { title: 1123, images: ["assets/pic/11.jpg", "assets/pic/1.jpg"], tagId: [4, 5] }
   ]
-  tags: any = [];
+  tags:any = [
+    { id: 1, title: "بدون چروک" },
+    { id: 2, title: "جلوبسته" },
+    { id: 3, title: "ابایی" },
+    { id: 4, title: "زیپی" },
+    { id: 5, title: "بدون دکمه" },
+    { id: 6, title: " بدون رنگ دهی" },
+    { id: 7, title: " جیبدار" },
+  ];
+  tagIdClothes=""
   detailPic: any = "";
   detailTitle: any = "";
   currentImge: string[] = [];
   currentIndex: number[] = [];
+
   constructor(private modalService: ModalService) {
     for (var i = 0; i < this.clothes.length; i++) {
       this.currentImge.push(this.clothes[i].images[0])
       this.currentIndex.push(0)
+      
     }
 
 
@@ -47,10 +58,13 @@ export class MainlistComponent implements OnInit {
     this.currentImge[clothesNum] = this.clothes[clothesNum].images[id];
     this.currentIndex[clothesNum] = id;
   }
-  showDetail(srcPic: any, titlePic: any,id:number) {
+  showDetail(srcPic: any, titlePic: any, id: number) {
+    this.tagIdClothes="";
     this.detailPic = srcPic;
     this.detailTitle = titlePic;
-    this.tags=this.clothes[id].tags;
+   this.tagIdClothes=this.clothes[id].tagId
+    console.log(this.tagIdClothes)
+    ///this.tags=this.clothes[id].tags;
   }
   openModal(id: string) {
 
