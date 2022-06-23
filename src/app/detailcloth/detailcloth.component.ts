c54a3c2af68660c67cc36a192e85a847347611ff
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-06-23 20:17:46 +0430

    in detail cloth
    set showImg()
    set number of pic
    set index

--- src/app/detailcloth/detailcloth.component.ts
+++ src/app/detailcloth/detailcloth.component.ts
@@ -6,19 +6,26 @@ import { ConnectorService } from '../connector.service';
   templateUrl: './detailcloth.component.html',
   styleUrls: ['./detailcloth.component.css']
 })
- 
-export class DetailclothComponent implements OnInit {
-id:any='';
-connector:any;
 
-  constructor(private route :ActivatedRoute ,svc:ConnectorService) { 
-    this.connector=svc.clothes;
-    
-    console.log(this.connector[0].title)
-  } 
+export class DetailclothComponent implements OnInit {
+  id: any = '';
+  connector: any;
+  selectedImg: any;
+  selectedIndex: any;
+  constructor(private route: ActivatedRoute, svc: ConnectorService) {
+    this.connector = svc.clothes;
+    this.selectedImg = svc.currentImge;
+    this.selectedIndex = svc.currentIndex
+
+  }
 
   ngOnInit(): void {
-    this.id=this.route.snapshot.paramMap.get('id')
+    this.id = this.route.snapshot.paramMap.get('id')
+
   }
-  
+  showImg(i: number) {
+    this.selectedImg[this.id] = this.connector[this.id].images[i]
+    this.selectedIndex[this.id] = i;
+
+  }
 }
