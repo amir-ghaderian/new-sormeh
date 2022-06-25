2088dd059133b4c2c76457c27798d2eec5765984
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-06-25 19:50:42 +0430

    set tag service

---
+++ src/app/tags.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagsService {
+  tags: any = [
+    { id: 0, title: "بدون چروک" },
+    { id: 1, title: "جلوبسته" },
+    { id: 2, title: "ابایی" },
+    { id: 3, title: "زیپی" },
+    { id: 4, title: "بدون دکمه" },
+    { id: 5, title: " بدون رنگ دهی" },
+    { id: 6, title: " جیبدار" },
+  ];
+ 
+}
