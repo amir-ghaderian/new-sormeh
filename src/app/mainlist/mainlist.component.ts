2be08cbf14c3cc9ac5787e386653a7eb89a220ac
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-07-09 21:15:10 +0430

    Update mainlist.component.ts

--- src/app/mainlist/mainlist.component.ts
+++ src/app/mainlist/mainlist.component.ts
@@ -20,23 +20,16 @@ export class MainlistComponent implements OnInit {
   product: any
   tagsCloth: Array<Tag>;
   selectedTags: Array<number> = [];
-  output: Array<number> | undefined
-  item: any
   filtered: number[] = []
   show = false;
   mainList: any
   constructor(private modalService: ModalService, svc: ConnectorService, svt: TagsService) {
     this.product = svc.clothes;
     this.tagsCloth = svt.tags;
-
     for (var i = 0; i < this.product.length; i++) {
       this.currentImge.push(this.product[i].images[0])
       this.currentIndex.push(0)
-
     }
-
-
-
   }
 
   ngOnInit(): void {
@@ -55,39 +48,25 @@ export class MainlistComponent implements OnInit {
       let location = this.selectedTags.lastIndexOf(id);
       this.selectedTags.splice(location, 1)
     }
-
     return this.selectedTags
-
   }
 
 
 
   setFilter() {
-
-
-    // const product = this.product.filter((c: any) => {
-    //   return c.tagId.indexOf(this.selectedTags) !== -1
-    // })
-    // console.log(product)
-
+    this.mainList = this.product
     this.filtered = []
-    for (var i = 0; i < this.selectedTags.length; i++) {
+    for (var i = 0; i <= this.selectedTags.length; i++) {
       for (var j = 0; j < this.mainList.length; j++) {
         for (var c = 0; c < this.mainList[j].tagId.length; c++) {
           if (this.selectedTags[i] === this.mainList[j].tagId[c]) {
             if (this.filtered.indexOf(this.mainList[j]) === -1) {
               this.filtered.push(this.mainList[j])
-
             }
-
-
           }
         }
       }
     }
     this.mainList = this.filtered
-    console.log(this.mainList)
-
   }
-
 }
