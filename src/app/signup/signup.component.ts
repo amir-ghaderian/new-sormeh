cad208ab9394460dbb0f59bac22f35018a6bc2e1
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-08-03 13:21:46 +0430

    set ngmodel for signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
-  Customer:any=[{
-    name: '',
-    email:'',
-    phoneNumber:0
-
-
-  }]
+  customer: Array<any> = []
   constructor() { }
+  @Input() newCustomer: any = {
+    customerName: "",
+    customerEmail: "",
+    phoneNmber: 0,
 
+  }
   ngOnInit(): void {
   }
-  addCustomer(){
-console.log(this.Customer)
+  addCustomer() {
+    this.customer.push (this.newCustomer);
+    console.log(this.customer)
   }
 
 }
